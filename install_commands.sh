5bd98ee85f7b07a51a2e19e025751283dc714af6
aaron cruces <aaroncruces@gmail.com>
2021-12-02 18:17:14 -0300

    React integration

--- install_commands.sh
+++ install_commands.sh
@@ -19,6 +19,20 @@ php artisan serve
 # install api rest client for php
 composer require guzzlehttp/guzzle 
 
+# activate npm for future frontend dependencies
+npm init
+
+# install helper for react scaffolding
+composer require laravel/ui
+php artisan ui react
+npm install resolve-url-loader --save-dev
+npm install && npm run dev
+
+# install browsersync for development
+npm install browser-sync browser-sync-webpack-plugin --save-dev
+
+
+
 # For the project to work on heroku, add the Procfile
 # also, generate some key, pruebamzzo is the name of the project
 heroku config:set APP_KEY=base64:ZxXrGribmCMrwS1hi+hfxCQKoWHh19qCCsF82QTL5As= -a pruebamzzo
---
+++ run_commands.sh
@@ -0,0 +1,6 @@
+# run dev server laravel
+php artisan serve &
+
+# run laravel with watcher for frontend changes.
+# must be run alongside "php artisan serve"
+npm run watch
